d8fb1d247d32e848ac51a1b70b172373ef729f29
denyAndriana0107 <denyandriana06@gmail.com>
2022-01-13 01:02:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Wisata.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+class Wisata {
+    var nama:String ?= null
+    var jarak:Double ?= null
+    var harga:Double ?= null
+    var kualitas:Double ?= null
+
+    constructor(nama: String,jarak:Double,harga:Double,kualitas:Double){
+        this.nama = nama
+        this.harga = harga
+        this.jarak = jarak
+        this.kualitas = kualitas
+    }
+}
